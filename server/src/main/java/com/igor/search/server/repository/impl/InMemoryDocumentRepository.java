ce63ebc8ae07108f817251d8208fa56adda0d102
Igor Dmitriev <igor.dmitriev@verygood.systems>
2018-02-27 08:14:28 +0200

    add concurrency

--- server/src/main/java/com/igor/search/server/repository/impl/InMemoryDocumentRepository.java
+++ server/src/main/java/com/igor/search/server/repository/impl/InMemoryDocumentRepository.java
@@ -17,6 +17,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import static java.util.stream.Collectors.toSet;
 
@@ -26,6 +28,10 @@ import static java.util.stream.Collectors.toSet;
 @Repository
 public class InMemoryDocumentRepository implements DocumentRepository {
 
+  private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+  private final Lock readLock = readWriteLock.readLock();
+  private final Lock writeLock = readWriteLock.writeLock();
+
   private static final String WORD_SPLIT_PATTERN = "\\s+";
 
   private final Map<String, Document> storage = new HashMap<>();
@@ -33,34 +39,54 @@ public class InMemoryDocumentRepository implements DocumentRepository {
 
   @Override
   public Document save(Document document) {
-    Document alreadyExistingDocument = storage.get(document.getKey());
-    if (alreadyExistingDocument != null) {
-      return alreadyExistingDocument;
-    }
+    writeLock.lock();
+    try {
+      Document alreadyExistingDocument = storage.get(document.getKey());
+      if (alreadyExistingDocument != null) {
+        return alreadyExistingDocument;
+      }
 
-    Set<String> words = new HashSet<>(Arrays.asList(document.getData().split(WORD_SPLIT_PATTERN)));
-    document.setCreatedAt(LocalDateTime.now());
-    words.forEach(word -> index.put(word, document));
-    storage.put(document.getKey(), document);
-    return document;
+      Set<String> words = new HashSet<>(Arrays.asList(document.getData().split(WORD_SPLIT_PATTERN)));
+      document.setCreatedAt(LocalDateTime.now());
+      words.forEach(word -> index.put(word, document));
+      storage.put(document.getKey(), document);
+      return document;
+    } finally {
+      writeLock.unlock();
+    }
   }
 
   @Override
   public Optional<Document> findByKey(String key) {
-    return Optional.ofNullable(storage.get(key));
+    readLock.lock();
+    try {
+      return Optional.ofNullable(storage.get(key));
+    } finally {
+      readLock.unlock();
+    }
   }
 
   @Override
   public Set<String> search(SearchCriteria searchCriteria) {
-    return searchCriteria.getTokens().stream()
-        .map(index::get)
-        .flatMap(Collection::stream)
-        .map(Document::getKey)
-        .collect(toSet());
+    readLock.lock();
+    try {
+      return searchCriteria.getTokens().stream()
+          .map(index::get)
+          .flatMap(Collection::stream)
+          .map(Document::getKey)
+          .collect(toSet());
+    } finally {
+      readLock.unlock();
+    }
   }
 
   @Override
   public long count() {
-    return storage.size();
+    readLock.lock();
+    try {
+      return storage.size();
+    } finally {
+      readLock.unlock();
+    }
   }
 }
